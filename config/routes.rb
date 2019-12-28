5c51a1ad76126ea5220af4c54fee593566685f1e
好川　諒生 <ryoseiyoshikawa@yoshikawaryouseinoMacBook-Pro.local>
2019-12-28 10:31:15 +0900

    Finish login

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: "tasks#index"
   resources :tasks
   resources :users
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
